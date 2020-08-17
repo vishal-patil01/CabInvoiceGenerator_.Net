58adee408a872c3d6d90cea448e6b7448d765ac5
vishal-patil01 <vishalpatil01@hotmail.com>
2020-08-17 18:08:54 +0530

    [Vishal]Added : Test & Code To Calculate Total Fare.

--- CabInvoiceGenerator/CabInvoiceGenerator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -11,5 +11,21 @@ namespace CabInvoiceGenerator
     /// </summary>
     public class CabInvoiceGenerator
     {
+        private static readonly double CostPerKilometer = 10.0;
+        private static readonly double CostPerMinute = 1.0;
+        private static readonly double MinimumFare = 5.0;
+        private double totalFare = 0.0;
+
+        /// <summary>
+        /// Function to Calculate Total Fare.
+        /// </summary>
+        /// <param name="distance">Total Distance.</param>
+        /// <param name="time">Total Time.</param>
+        /// <returns>Total Fare.</returns>
+        public double CalculateFare(double distance, double time)
+        {
+            this.totalFare = (distance * CostPerKilometer) + (time * CostPerMinute);
+            return Math.Max(this.totalFare, MinimumFare);
+        }
     }
 }
--- CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -4,6 +4,7 @@
 
 namespace CabInvoiceGeneratorTest
 {
+    using CabInvoiceGenerator;
     using NUnit.Framework;
 
     /// <summary>
@@ -11,12 +12,39 @@ namespace CabInvoiceGeneratorTest
     /// </summary>
     public class CabInvoiceGeneratorTest
     {
+        private CabInvoiceGenerator cabInvoiceGenerator;
+
         /// <summary>
-        /// Setup Method For Initializing Instance of Class for Test Methods.
+        /// Setup Method For Initializing Instance of <see cref="CabInvoiceGenerator"/> Class for Test Methods.
         /// </summary>
         [SetUp]
         public void Setup()
         {
+            this.cabInvoiceGenerator = new CabInvoiceGenerator();
+        }
+
+        /// <summary>
+        /// Craete Test For Calculate Total Fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_WhenTotalFareIsGreaterThanMinimumFare_ShouldReturnTotalFare()
+        {
+            double distance = 5.0;
+            int time = 5;
+            double totalFare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(55, totalFare);
+        }
+
+        /// <summary>
+        /// Craete Test For Calculate Minimum Fare.
+        /// </summary>
+        [Test]
+        public void GivenLessDistanceAndTime_WhenTotalFareIsLessThanMinimumFare_ShouldReturnMinimumFare()
+        {
+            double distance = 0.1;
+            int time = 1;
+            double totalFare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(5.0d, totalFare);
         }
     }
 }
