b310d564371aa7df60a33bea80e0d59204068bdf
vishal-patil01 <vishalpatil01@hotmail.com>
2020-08-17 17:28:01 +0530

    Add project files.

---
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="CabInvoiceGenerator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGenerator
+{
+    using System;
+
+    /// <summary>
+    /// Main Class To Generate Cab Invoice.
+    /// </summary>
+    public class CabInvoiceGenerator
+    {
+    }
+}
---
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,22 @@
+// <copyright file="CabInvoiceGeneratorTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGeneratorTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test Class To Test Cab Invoice Generator.
+    /// </summary>
+    public class CabInvoiceGeneratorTest
+    {
+        /// <summary>
+        /// Setup Method For Initializing Instance of Class for Test Methods.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
